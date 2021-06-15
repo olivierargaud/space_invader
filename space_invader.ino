63835ecc0812193d730ae284642b5530ff209f25
olivierargaud <olivier.argaud@ipilyon.net>
2021-06-15 09:32:16 +0200

    debut du projet

--- space_invader.ino
+++ space_invader.ino
@@ -1,9 +1,70 @@
+
+
+#include <Gamebuino-Meta.h>
+
+
+
+int widthEcran = 79;
+int heightEcran = 63;
+
+
+int posVaisseaux = 30;
+int vitVaisseaux = 0;
+
+boolean rightButton = false;
+boolean leftButton = false;
+
 void setup() {
-  // put your setup code here, to run once:
+  
+  gb.begin();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  while(!gb.update());
+  gb.display.clear();
 
+  mouvementVaissaux();
+  affiche();
+
+}
+
+void mouvementVaissaux(){
+
+
+  if(gb.buttons.pressed(BUTTON_LEFT)){
+    leftButton = true;
+  }
+
+   if(gb.buttons.released(BUTTON_LEFT)){
+    leftButton = false;
+  }
+
+   if(gb.buttons.pressed(BUTTON_RIGHT)){
+    rightButton = true;
+  }
+
+   if(gb.buttons.released(BUTTON_RIGHT)){
+    rightButton = false;
+  }
+
+
+  if(rightButton && posVaisseaux < widthEcran-16){
+    posVaisseaux++;
+  }
+  if(leftButton && posVaisseaux > 1){
+    posVaisseaux--;
+  }
+
+}
+
+
+void affiche(){
+
+
+  // dessine le vaisseaux
+  gb.display.setColor(BLUE);
+  gb.display.fillRect(posVaisseaux, 54, 16, 4);
+  
 }
